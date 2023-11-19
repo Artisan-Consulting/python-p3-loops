8b246c0d97c7c8f99c9edad8a7126c077b0c7f51
Artisan <william.kwan@artisanconsultin.nz>
2023-11-19 19:46:52 +1100

    loop statements added

--- lib/looping.py
+++ lib/looping.py
@@ -1,13 +1,30 @@
 #!/usr/bin/env python3
 
+
 def happy_new_year():
-    # code goes here!
-    pass
+    i = 1
+    while i < 11:
+        print (f'{i}\n')
+        i += 1
+        print ("Happy New Year!")
+
 
 def square_integers(int_list):
-    # code goes here!
-    pass
+    result = []
+    for value in int_list:
+        squared = abs(value ** 2)
+        result.append(squared)
+    return result
 
 def fizzbuzz():
-    # code goes here!
-    pass
+    # prints 1 to 100 with fizz 3x, buzz 5s, fizzbuzz 3and5s
+    for i in range(1, 101):
+        if i % 3 == 0 and i % 5 == 0:
+            print ("FizzBuzz")
+        elif i % 3 == 0:
+            print ("Fizz")
+        elif i % 5 == 0:
+            print ("Buzz")
+        else:
+            print (i)
+      
